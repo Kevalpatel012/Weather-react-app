51b63eaf2e973ba8389a5558e3eb1729fe99e4c0
Keval Patel <kp005919@ohio.edu>
2023-03-22 17:07:04 -0400

    first commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: "GET",
+	headers: {
+	  "X-RapidAPI-Key": "1bf2005310mshc0bed0e881af7c8p17cdf9jsn0f7c1f276f89",// enter your rapid api key here
+	  "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+	},
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "60c4380dc85e71da3aee8fe194fdb1a0"; // enter your key from openweather API
+  
